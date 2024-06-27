abbf6007dd3ed07fa19bbc1779e608df02f64c76
48592151 <sofiamossa12@gmail.com>
2024-06-27 10:16:18 -0300

    script producto

--- Assets/Scripts/SumaPrecios.cs
+++ Assets/Scripts/SumaPrecios.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class SumaPrecios : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        InizializarJuego();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+      
+
+    }
+    void InizializarJuego()
+    {
+
     }
 }
