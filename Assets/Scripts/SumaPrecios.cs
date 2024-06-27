2ea8b31755da05af84d2694b6fc9c8246824667a
48592151 <sofiamossa12@gmail.com>
2024-06-27 16:37:35 -0300

    Actualzación de precios

--- Assets/Scripts/SumaPrecios.cs
+++ Assets/Scripts/SumaPrecios.cs
@@ -15,9 +15,8 @@ public class SumaPrecios : MonoBehaviour
     public GameObject respuesta;
     public GameObject respuestaCorrecta;
     public GameObject respuestaIncorrecta;
-    public GameObject inputVacio;
-    int factorIzquierdo;
-    int factorDerecho;
+    int precio1;
+    int precio2;
     // Start is called before the first frame update
     void Start()
     {
@@ -39,20 +38,20 @@ public class SumaPrecios : MonoBehaviour
             producto2[i].SetActive(false);
             respuestaIncorrecta.SetActive(false);
             respuestaCorrecta.SetActive(false);
-            inputVacio.SetActive(false);
+           
         }
     }
 
     void Active()
     {
-        factorIzquierdo = Random.Range(0, producto1.Length);
-        factorDerecho = Random.Range(0, producto1.Length);
-        producto1[factorIzquierdo].transform.position = new Vector3(6f, 0f, 0f);
-        producto2[factorDerecho].transform.position = new Vector3(-6f, 0f, 0f);
-        producto1[factorIzquierdo].SetActive(true);
-        producto2[factorDerecho].SetActive(true);
-        int Precio1 = producto1[factorIzquierdo].GetComponent<ProductoDer>().precio;
-        int Precio2 = producto2[factorDerecho].GetComponent<ProductoDer>().precio;
+        precio1 = Random.Range(0, producto1.Length);
+        precio2 = Random.Range(0, producto1.Length);
+        producto1[precio1].transform.position = new Vector3(6f, 0f, 0f);
+        producto2[precio2].transform.position = new Vector3(-6f, 0f, 0f);
+        producto1[precio1].SetActive(true);
+        producto2[precio2].SetActive(true);
+        int Precio1 = producto1[precio1].GetComponent<ProductoDer>().precio;
+        int Precio2 = producto2[precio2].GetComponent<ProductoDer>().precio;
         TextoSimboloDePesos1.text = "$" + Precio1.ToString();
         TextoSimboloDePesos2.text = "$" + Precio2.ToString();
     }
@@ -60,24 +59,21 @@ public class SumaPrecios : MonoBehaviour
     {
         Text respuestaText = respuesta.GetComponentInChildren<Text>();
 
-        if (respuestaText != null && respuestaText.text == "")
-        //if (respuesta.text == "") //me tiraba error text
+        if (respuestaText != null && string.IsNullOrEmpty(respuestaText.text))
         {
-            inputVacio.SetActive(true);
             respuestaCorrecta.SetActive(false);
             respuestaIncorrecta.SetActive(false);
         }
-        else if(respuestaText != null)
+        else if (respuestaText != null)
         {
-            inputVacio.SetActive(false);
-            int Precio1 = producto1[factorIzquierdo].GetComponent<ProductoDer>().precio;
-            int Precio2 = producto2[factorDerecho].GetComponent<ProductoDer>().precio;
+            int Precio1 = producto1[precio1].GetComponent<ProductoDer>().precio;
+            int Precio2 = producto2[precio2].GetComponent<ProductoDer>().precio;
             int precioTotal = Precio1 + Precio2;
             int respuestaUser;
 
-            if(int.TryParse(respuestaText.text, out respuestaUser))
+            if (int.TryParse(respuestaText.text, out respuestaUser))
             {
-                if(respuestaUser == precioTotal)
+                if (respuestaUser == precioTotal)
                 {
                     respuestaCorrecta.SetActive(true);
                     respuestaIncorrecta.SetActive(false);
@@ -90,17 +86,14 @@ public class SumaPrecios : MonoBehaviour
             }
             else
             {
-                inputVacio.SetActive(true);
                 respuestaCorrecta.SetActive(false);
                 respuestaIncorrecta.SetActive(false);
             }
         }
     }
 
-    void botonCerrar()
-    {
-        inputVacio.SetActive(false);
-    }
+
+
 
     public void botonVolverAIntentar()
     {
